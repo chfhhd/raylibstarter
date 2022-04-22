1569bbd7c1768ed220733a29abbb9b9107a1d21b
Christoph Hahn <chrhhn@gmail.com>
2022-04-22 15:49:46 +0200

    Documented Actor Class

--- src/core/actor.h
+++ src/core/actor.h
@@ -3,6 +3,14 @@
 #include "sprite.h"
 
 namespace game::core {
+    /**
+     * Actors can be placed in the actor-map of each scene class and will be drawn automatically if their visible
+     * attribute is true. If actor objects are supposed to be available across scenes, they can be stored in
+     * game::core::actors. Objects that are only referenced there will not be drawn automatically.
+     * The position of an actor object in the game depends on the position of its sprite attribute.
+     *
+     * @brief Actor base class. Actors represent all game objects, such as players, enemies and other obstacles.
+     */
     class Actor {
     private:
         std::shared_ptr<game::core::Sprite> sprite_;
@@ -10,6 +18,11 @@ namespace game::core {
     public:
         Actor() = delete;
 
+        /**
+         * Create a new Actor object.
+         * @brief Constructor.
+         * @param sprite Sprite object that is assigned to the Actor.
+         */
         explicit Actor(std::shared_ptr<game::core::Sprite> sprite);
 
         Actor(const game::core::Actor &actor) = delete;
@@ -18,8 +31,14 @@ namespace game::core {
 
         virtual ~Actor();
 
+        /**
+         * @brief Returns the sprite object of the actor
+         */
         [[nodiscard]] const std::shared_ptr<game::core::Sprite> &sprite() const;
 
+        /**
+         * @brief Replaces the current sprite object of the actor.
+         */
         void sprite(std::shared_ptr<game::core::Sprite> sprite);
     };
 }
