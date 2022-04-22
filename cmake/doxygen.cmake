307151c475a07ae9602e41e08f775f871c83576c
Christoph Hahn <chrhhn@gmail.com>
2022-04-22 23:49:13 +0200

    Added the Doxygen Awesome CSS theme

--- cmake/doxygen.cmake
+++ cmake/doxygen.cmake
@@ -1,4 +1,4 @@
-# Source: https://vicrucann.github.io/tutorials/quick-cmake-doxygen/
+# Based on: https://vicrucann.github.io/tutorials/quick-cmake-doxygen/
 
 # First we can indicate the documentation build as an option and set it to ON by default
 option(BUILD_DOC "Build documentation" ON)
@@ -9,6 +9,11 @@ if (DOXYGEN_FOUND)
     # Set input and output files
     set(DOXYGEN_IN ${CMAKE_CURRENT_SOURCE_DIR}/../docs/Doxyfile.in)
     set(DOXYGEN_OUT ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile)
+
+    # Include doxygen-awesome.css
+    file(DOWNLOAD
+            https://raw.githubusercontent.com/jothepro/doxygen-awesome-css/main/doxygen-awesome.css
+            ${CMAKE_CURRENT_BINARY_DIR}/doxygen-awesome.css)
 
     # Request to configure the file
     configure_file(${DOXYGEN_IN} ${DOXYGEN_OUT} @ONLY)
