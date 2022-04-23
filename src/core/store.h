a806dd8902c8bdeaee660fc6957bc47e6d8b99a0
Christoph Hahn <chrhhn@gmail.com>
2022-04-23 13:16:14 +0200

    Documented Store class

--- src/core/store.h
+++ src/core/store.h
@@ -12,12 +12,17 @@ namespace game::core {
      * @brief A central place to share globally accessible objects that should be accessible from anywhere.
      */
     struct Store final {
-         inline static std::unique_ptr<game::core::Stage> stage = nullptr;
+        /// The Stage object is responsible for the scene change and for updating and drawing the scene contents
+        inline static std::unique_ptr<game::core::Stage> stage = nullptr;
 
-         inline static Vector2 mouse_position = { };
+        /// @brief The virtual mouse position.
+        /// To get the mouse position the mouse support must be enabled when initializing the Game object.
+        inline static Vector2 mouse_position = { };
 
-         inline static long long int ticks = 1;
+        /// Global game counter. Can be used e.g. for the calculation of animation times.
+        inline static long long int ticks = 1;
 
-         inline static std::map<std::string, std::shared_ptr<game::core::Actor>> actors = { };
+        /// The actors map can contain shared pointers to actor objects that are to be used across multiple scenes (e.g. the player actor).
+        inline static std::map<std::string, std::shared_ptr<game::core::Actor>> actors = { };
     };
 }
